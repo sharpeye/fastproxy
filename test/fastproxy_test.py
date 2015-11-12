081ba8b377024d520940f43c16643f4cb1a2e472
Sergey Pimenov <cepera@yandex-team.ru>
2015-11-13 02:42:00 +0300

    Use process group to kill all 'fastproxy' processes during tests tear down and avoid problem with child processes left.

--- test/fastproxy_test.py
+++ test/fastproxy_test.py
@@ -3,11 +3,13 @@ Created on Jul 8, 2010
 
 @author: nbryskin
 '''
+import os
 import unittest
-from subprocess import Popen
+import subprocess
 import urllib
 import time
 import socket
+import signal
 
 class Test(unittest.TestCase):
     port = 32567
@@ -16,16 +18,14 @@ class Test(unittest.TestCase):
     stat_sock = '/tmp/stat.sock'
 
     def setUp(self):
-        self.fastproxy = Popen('../build/debug/src/fastproxy \
-            --ingoing-http=127.0.0.1:{0} --receive-timeout={1}\
-            --name-server=95.108.198.4 --allow-header={2} --ingoing-stat={3}'.format(self.port, self.timeout, self.allowed_header, self.stat_sock),
-            shell=True, env={'LD_LIBRARY_PATH': '/usr/local/lib64'})
+        self.fastproxy = subprocess.Popen('../build/debug/src/fastproxy \
+            --ingoing-http=127.0.0.1:{0} --receive-timeout={1} --resolve-library=udns \
+            --udns-name-server=95.108.198.4 --allow-header={2} --ingoing-stat={3}'.format(self.port, self.timeout, self.allowed_header, self.stat_sock),
+            shell=True, env={'LD_LIBRARY_PATH': '/usr/local/lib64'}, preexec_fn=os.setsid)
         time.sleep(1)
 
     def tearDown(self):
-        #pass
-        self.fastproxy.terminate()
-        self.fastproxy.wait()
+        os.killpg(self.fastproxy.pid, signal.SIGTERM)
 
     def test_running(self):
         self.assertFalse(self.fastproxy.poll())
@@ -35,7 +35,7 @@ class Test(unittest.TestCase):
 
     def test_simple_ip(self):
         urllib.urlopen('http://77.88.21.3', proxies={'http': 'http://localhost:{0}'.format(self.port)})
-        
+
     def _send_request(self, headers=None, method='GET', host='localhost'):
         l = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         l.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -48,7 +48,7 @@ class Test(unittest.TestCase):
         l.settimeout(1)
         s, addr = l.accept()
         return s.recv(len(request))
-        
+
     def test_http(self):
         request = self._send_request()
         self.assertEqual(request, 'GET / HTTP/1.0\r\n\r\n')
@@ -64,7 +64,7 @@ class Test(unittest.TestCase):
         time.sleep(self.timeout)
         self.assertFalse(self.fastproxy.poll())
         self.assertRaises(BaseException, self.c.send('a'))
-        
+
     def test_header_filter(self):
         allowed_header = '{0}: test\r\n'.format(self.allowed_header)
         disallowed_header = 'DisAllowedHeader: test\r\n'
@@ -95,18 +95,16 @@ class Test(unittest.TestCase):
     def test_statistics(self):
         self.stat = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.stat.connect(self.stat_sock)
-        
+
         self.test_http()
         self.c.close()
         self.stat.send('total_sessions current_sessions total_stat_sessions current_stat_sessions unexisting_stat\n')
         self.assertEqual(self.stat.recv(64), '1\t0\t1\t1\tunexisting_stat?\n')
-        
+
         self.test_http()
         self.c.close()
         self.stat.send('total_sessions current_sessions total_stat_sessions current_stat_sessions unexisting_stat\n')
         self.assertEqual(self.stat.recv(64), '2\t0\t1\t1\tunexisting_stat?\n')
 
 if __name__ == "__main__":
-    import sys
-    #sys.argv = ['', 'Test.test_simple_ip']
     unittest.main()
